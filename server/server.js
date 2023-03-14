3a0353ec1dafa817a63ac3ff0fa47682e7b96126
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-03-14 10:17:18 +0200

    add mongodb setup code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { MongoClient } = require('mongodb')
+const dbUrl = 'mongodb://db:27017'
+const client = new MongoClient(dbUrl)
+
+async function main() {
+    await client.connect()
+    console.log("connected to database")
+    const db = client.db("website")
+    const properties = db.collection("properties")
+
+
+    return 'done'
+}
+
+main().then(console.log).catch(cosole.error).finally(() => client.close());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
