770e0b862240aa8e66deff3b593b9b33f30e5b96
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-04-05 10:46:43 +0300

    move icons and styles to first level folders

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/public'))
+app.use("/icons", express.static(__dirname + '/icons'))
+app.use("/styles", express.static(__dirname + '/styles'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/view/home.html")
 })
-  
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
