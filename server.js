c47fb01830cf6748838a57008b10dd020181e694
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-04-21 18:44:40 +0300

    convert pages to use handlebars

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express')
-const hbs = require('express-handlebars')
+const {create} = require('express-handlebars')
 const port = 3000
 
 // Setup express and handlebars
 const app = express()
-app.engine(
-    'hbs',
-    hbs({
-        defaultLayout: 'main',
-        extname: '.hbs'
-    })
-)
+const hbs = create({
+    defaultLayout: false,
+    extname: '.hbs'
+})
+
+app.engine( 'hbs', hbs.engine)
 app.set('view engine', 'hbs')
 
-// Add static folders
+// Configure static folders
 app.use(express.static(__dirname + '/public'))
 app.use("/icons", express.static(__dirname + '/icons'))
 app.use("/styles", express.static(__dirname + '/styles'))
 
+// Define routes
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/view/home.html")
+    res.render("home")
 })
 
 app.get('/sign_in', (req, res) => {
-    res.sendFile(__dirname + "/view/sign_in.html")
+    res.render("sign_in")
 })
 
 app.get('/sign_up', (req, res) => {
-    res.sendFile(__dirname + "/view/sign_up.html")
+    res.render("sign_up")
 })
 
+// Run server
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
