a4f81351b4494ffc36fd57eba372e491a4bb43e0
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-04-30 16:16:50 +0300

    create database connection and listing model

--- models/listing.js
+++ models/listing.js
@@ -8,10 +8,54 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err,
 const db = mongoose.connection;
 
 const listingSchema = new mongoose.Schema({
-	type: String,
-	location: String,
-	price: double,
-	area: double
+	type: { type: String, required: true },
+	location: { type: String, required: true },
+	price: { type: Number, required: true },
+	area: { type: Number, required: true },
+	extra: { type: Object }
 });
 
-const Listing = mongoose.model("Listing", listingSchema, "Listings");
+export const Listing = mongoose.model("Listing", listingSchema, "Listings");
+
+export async function getListings(type, location, priceStart=-1, priceEnd=-1, areaStart=-1, areaEnd=-1) {
+	let querry = {
+		type: type,
+		location: location
+	}
+
+	if (priceStart != -1 && priceEnd != -1) {
+		querry.price = {
+			$and: {
+				$gt: priceStart,
+				$lt: priceEnd,
+			}
+		}
+	} else if (priceEnd == -1) {
+		querry.price = {
+			$gt: priceStart,
+		}
+	} else if (priceStart == -1) {
+		querry.price = {
+			$lt: priceEnd,
+		}
+	}
+
+	if (areaStart != -1 && areaEnd != -1) {
+		querry.price = {
+			$and: {
+				$gt: areaStart,
+				$lt: areaEnd,
+			}
+		}
+	} else if (areaEnd == -1) {
+		querry.price = {
+			$gt: areaStart,
+		}
+	} else if (areaStart == -1) {
+		querry.price = {
+			$lt: areaEnd,
+		}
+	}
+
+	return await Listing.find(querry).exec()
+}
