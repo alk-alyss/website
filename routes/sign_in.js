82980ac9b7e840675bda1edebce318f3a596b4f0
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-01 22:58:02 +0300

    migrate to using layouts, pass css file as data

--- routes/sign_in.js
+++ routes/sign_in.js
@@ -2,11 +2,11 @@ import { Router } from "express"
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render("sign_in")
+    res.render("sign_in", { style: "home/sign_in" })
 })
 
 router.post('/', (req, res) => {
-    res.render("sign_in")
+    res.render("sign_in", { style: "home/sign_in" })
 })
 
 export default router
--- routes/sign_up.js
+++ routes/sign_up.js
@@ -2,11 +2,11 @@ import { Router } from "express"
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render("sign_up")
+    res.render("sign_up", {style: "home/sign_in"})
 })
 
 router.post('/', (req, res) => {
-    res.render("sign_up")
+    res.render("sign_up", {style: "home/sign_in"})
 })
 
 export default router
