63e45f5fc0b6bc9307d2bfe18a83f87f843a5558
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-05 12:13:40 +0300

    fix style, redirect if listing does not exist

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,14 +1,18 @@
 import * as listing from "../models/listing.js"
 
 export async function showListing(req, res, next) {
-	let id = req.params.id
+	let id = req.params.id.toString()
 
 	let currentListing = await listing.getListingById(id)
+
+	if (!currentListing) {
+		res.status(404).redirect("/")
+	}
 
 	console.log(currentListing)
 
 	res.render("listing", {
-		style: "search",
+		style: "listing",
 		topSearchOn: true,
 		listing: currentListing
 	})
