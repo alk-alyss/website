972ca5161ee0945a231181cfc8f60ea607e6dab9
DimimiS <dimiskama115@gmail.com>
2023-05-10 17:40:06 +0300

    backend get sign in/up info

--- routes/sign_in.js
+++ routes/sign_in.js
@@ -9,4 +9,13 @@ router.post('/', (req, res) => {
     res.render("sign_in", { style: "sign_in" })
 })
 
+router.post('/signing_in', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (username == "admin" && password == "admin") {
+        // scenario for admin user
+    }
+})
+
 export default router
--- routes/sign_up.js
+++ routes/sign_up.js
@@ -9,4 +9,10 @@ router.post('/', (req, res) => {
     res.render("sign_up", {style: "sign_in"})
 })
 
+router.post('/signing_up', (req, res) => {
+    let username = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+})
+
 export default router
