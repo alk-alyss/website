6aacef13c5f9c89f74c548d348c5f54fb07331cd
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-11 13:49:29 +0300

    hash passwords before storing in database

--- controllers/sign_up.js
+++ controllers/sign_up.js
@@ -1,11 +1,14 @@
-import { addUser } from "../models/user";
+import bcrypt from "bcrypt"
+import { addUser } from "../models/user.js";
 
 export async function sign_up(req, res) {
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
 
-    let result = await addUser(username, email, password)
+    let hash = await bcrypt.hash(password, 10)
+
+    let result = await addUser(username, email, hash)
 
     if (result == "email") {
         return
--- controllers/startup.js
+++ controllers/startup.js
@@ -1,6 +1,8 @@
-import { importListings } from "../models/listing.js"
-import { importUsers } from "../models/user.js"
+import bcrypt from "bcrypt"
 import fs from "fs/promises"
+
+import { importListings } from "../models/listing.js"
+import { importUsers, getUsers } from "../models/user.js"
 
 export async function loadData() {
 	let listingsFile = await fs.readFile("./data/listings.json")
@@ -10,6 +12,11 @@ export async function loadData() {
 
 	let usersFile = await fs.readFile("./data/users.json")
 	let usersJson = JSON.parse(usersFile)
+
+	for (let user of usersJson) {
+		let hash = await bcrypt.hash(user.password, 10)
+		user.password = hash
+	}
 	
 	await importUsers(usersJson)
 }
