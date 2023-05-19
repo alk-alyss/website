b8f0ab4183ebc80313a376bc35f3ce7b6477632d
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-19 21:43:42 +0300

    add ground work for adding and removing favorite listings

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,5 @@
 import { getListingById } from "../models/listing.js"
+import { addFavoriteListing, getUserByUsername, removeFavoriteListing } from "../models/user.js"
 
 export async function showListing(req, res, next) {
 	let id = req.params.id.toString()
@@ -16,4 +17,19 @@ export async function showListing(req, res, next) {
 		topSearchOn: true,
 		listing: currentListing
 	})
+}
+
+export async function toggleListing(req, res) {
+	let username = req.session.username
+	let id = req.params.id.toString()
+
+	let user = await getUserByUsername(username)
+	if (user.favoriteListings.includes(id)) {
+		removeFavoriteListing(username, id)
+	} else {
+		addFavoriteListing(username, id)
+	}
+
+	let referrer = req.get("Referer")
+	console.log(referrer)
 }
--- routes/listing.js
+++ routes/listing.js
@@ -2,7 +2,10 @@ import { Router } from "express"
 const router = Router()
 
 import { showListing } from "../controllers/listing.js"
+import { checkAuthentication } from "../controllers/authentication.js"
 
 router.get('/:id', showListing)
+
+router.post('/:id/heart', checkAuthentication, toggleHeart)
 
 export default router
