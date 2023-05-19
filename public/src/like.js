0d50f46a122599c8361bf0248a35b46b64dab9b6
DimimiS <dimiskama115@gmail.com>
2023-05-19 16:37:28 +0300

    commented like.js for user profile liked implementation

--- public/src/like.js
+++ public/src/like.js
@@ -8,8 +8,10 @@ items.forEach(item => {
         // check if signed in
         // if not signed in, redirect to sign in page
         // else, add to users liked list
+        // if already liked, remove from liked list
         
-        if (item.checked) {
-        }
+        
+        // while window loading, if item is in users liked list, change checkbox to checked
+        // if item is not liked, change color to checkbox unchecked
     });
 });
