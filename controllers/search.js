76cffcc0dc99a1290bb28bdef379c279cb308646
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-05-25 19:28:12 +0300

    organize filters better

--- controllers/search.js
+++ controllers/search.js
@@ -86,39 +86,59 @@ export async function search(req, res, next) {
 
     let priceStart = Number(req.query.price_start)
     let priceEnd = Number(req.query.price_end)
+
+    filters.price = {
+        start: priceStart > 0 ? priceStart : 0,
+        end: priceEnd > 0 ? priceEnd : 0
+    }
+
     let areaStart = Number(req.query.area_start)
     let areaEnd = Number(req.query.area_end)
 
-    filters.priceStart = priceStart > 0 ? priceStart : 0
-    filters.priceEnd = priceEnd > 0 ? priceEnd : 0
-    filters.areaStart = areaStart > 0 ? areaStart : 0
-    filters.areaEnd = areaEnd > 0 ? areaEnd : 0
+    filters.area = {
+        start: areaStart > 0 ? areaStart : 0,
+        end: areaEnd > 0 ? areaEnd : 0
+    }
 
-    filters.stateNew = req.query.new != undefined
-    filters.stateRenovated = req.query.renovated != undefined
-    filters.stateGood = req.query.good != undefined
-    filters.stateOld = req.query.old != undefined
+    filters.state = {
+        new: req.query.new != undefined,
+        renovated: req.query.renovated != undefined,
+        good: req.query.good != undefined,
+        old: req.query.old != undefined
+    }
 
     let roomsFrom = Number(req.query.rooms_from)
     let roomsTo = Number(req.query.rooms_to)
+
+    filters.rooms = {
+        start: roomsFrom > 0 ? roomsFrom : 0,
+        end: roomsTo > 0 ? roomsTo : 0
+    }
+
     let floorFrom = Number(req.query.floor_from)
     let floorTo = Number(req.query.floor_to)
+
+    filters.floors = {
+        start: floorFrom > 0 ? floorFrom : 0,
+        end: floorTo > 0 ? floorTo : 0
+    }
+
     let yearFrom = Number(req.query.year_from)
     let yearTo = Number(req.query.year_to)
 
-    filters.roomsFrom = roomsFrom > 0 ? roomsFrom : 0
-    filters.roomsTo = roomsTo > 0 ? roomsTo : 0
-    filters.floorFrom = floorFrom > 0 ? floorFrom : 0
-    filters.floorTo = floorTo > 0 ? floorTo : 0
-    filters.yearFrom = yearFrom > 0 ? yearFrom : 0
-    filters.yearTo = yearTo > 0 ? yearTo : 0
+    filters.year = {
+        start: yearFrom > 0 ? yearFrom : 0,
+        end: yearTo > 0 ? yearTo : 0
+    }
 
-    filters.home = req.query.home != undefined
-    filters.commercial = req.query.commercial != undefined
-    filters.industrial = req.query.industrial != undefined
-    filters.agricultural = req.query.agricultural != undefined
-    filters.forest = req.query.forest != undefined
-    filters.tourist = req.query.tourist != undefined
+    filters.zone = {
+        home: req.query.home != undefined,
+        commercial: req.query.commercial != undefined,
+        industrial: req.query.industrial != undefined,
+        agricultural: req.query.agricultural != undefined,
+        forest: req.query.forest != undefined,
+        tourist: req.query.tourist != undefined
+    }
 
     filters.heating = req.query.heating
     switch (filters.heating) {
@@ -155,21 +175,23 @@ export async function search(req, res, next) {
             break
     }
 
-    filters.parking = req.query.parking != undefined
-    filters.garden = req.query.garden != undefined
-    filters.balcony = req.query.balcony != undefined
-    filters.storage = req.query.storage != undefined
-    filters.fireplace = req.query.fireplace != undefined
-    filters.elevator = req.query.elevator != undefined
-    filters.furnished = req.query.furnished != undefined
-    filters.air_condition = req.query.air_condition != undefined
-    filters.solar_heating = req.query.solar_heating != undefined
-    filters.boiler = req.query.boiler != undefined
-    filters.alarm = req.query.alarm != undefined
-    filters.security_door = req.query.security_door != undefined
-    filters.pets = req.query.pets != undefined
-    filters.student = req.query.student != undefined
-    filters.loading_dock = req.query.loading_dock != undefined
+    filters.extra = {
+        parking: req.query.parking != undefined,
+        garden: req.query.garden != undefined,
+        balcony: req.query.balcony != undefined,
+        storage: req.query.storage != undefined,
+        fireplace: req.query.fireplace != undefined,
+        elevator: req.query.elevator != undefined,
+        furnished: req.query.furnished != undefined,
+        air_condition: req.query.air_condition != undefined,
+        solar_heating: req.query.solar_heating != undefined,
+        boiler: req.query.boiler != undefined,
+        alarm: req.query.alarm != undefined,
+        security_door: req.query.security_door != undefined,
+        pets: req.query.pets != undefined,
+        student: req.query.student != undefined,
+        loading_dock: req.query.loading_dock != undefined
+    }
 
     let listings = await getListings(filters)
 
